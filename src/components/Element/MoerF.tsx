39d4c115d40c9525f980ca0a0cdec4da0036aa23
TheKorn01 <101316213+TheKorn01@users.noreply.github.com>
2023-09-15 15:04:04 +0700

    TT-Bar-V.2

--- src/components/Element/MoerF.tsx
+++ src/components/Element/MoerF.tsx
@@ -6,7 +6,9 @@ const Sbar = ({More}:any) => {
     <div className='px-[8px] py-[10px]'>
         <div className='flex h-[50px] py-[2px] border-2 rounded-[13px] border-[#818487]'>
             <div className='flex pl-[9px] '>
-                <div className='bg-[#818487] h-[42px] w-[42px] rounded-full'/>
+                <div className='bg-[#818487] h-[42px] w-[42px] rounded-full'>
+                    
+                </div>
                 <div className='py-[10px] pl-[10px] w-[154px] items-center'>
                     <p className={styles.header}>{More}</p>
                 </div>
